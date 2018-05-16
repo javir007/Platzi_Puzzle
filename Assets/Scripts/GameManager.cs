b76bc530afa3f4c2a2d2ab4cb37270865446e7a9
Javir <javir.urro01@gmail.com>
2018-05-16 16:34:03 -0500

    creando el GameManager (logica de juego) parte 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    private int points = 0;
+    private int itemAmnt = 0;
+    private int playerLifes = 3;
+
+	private void Awake()
+	{
+        if(instance == null){
+            instance = this;
+        }else if(instance != this){
+            Destroy(gameObject);
+        }
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PickItem(){
+        points += 10;
+        itemAmnt++;
+
+    }
+
+    public void LoseLife(){
+        if(playerLifes > 1){
+            playerLifes--;
+        }else{
+            playerLifes = 0;
+        }
+
+    }
+
+}
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,6 +7,7 @@ public class Item : MonoBehaviour {
 	private void OnTriggerEnter(Collider other)
 	{
         if(other.CompareTag("Player")){
+            GameManager.instance.PickItem();
             Destroy(gameObject);
         }
 	}
